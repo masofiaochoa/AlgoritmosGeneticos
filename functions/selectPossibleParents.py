e0b6fdf2b3390ffd8a8a63ca396b9e39e7d92314
MQRLZ <martinez34950@gmail.com>
2025-05-16 20:27:29 -0300

    Implemented tournament selection method. Has unexpected success in maximizing the objective function. Left prints for testing.

--- functions/selectPossibleParents.py
+++ functions/selectPossibleParents.py
@@ -5,6 +5,7 @@ from config import *
 from individual import Individual
 from enum_method import Method
 from .roulette import roulette
+from .tournament import tournament
 
 def selectPossibleParents(method: Method, population: list[Individual]) -> list[Individual]:
     
@@ -13,8 +14,7 @@ def selectPossibleParents(method: Method, population: list[Individual]) -> list[
     if method == Method.ROULETTE:
         selectedPossibleParents = roulette(population)
     elif method == Method.TOURNAMENT:
-        # selectedPossibleParents = tournament(population)
-        pass
+        selectedPossibleParents = tournament(population)
 
     return selectedPossibleParents;
 
---
+++ functions/tournament.py
@@ -0,0 +1,37 @@
+import random
+
+from individual import Individual
+from config import *
+
+def tournament(population: list[Individual]) -> list[Individual]:
+    
+    selectedPossibleParents: list[Individual] = []
+
+    while (len(selectedPossibleParents) < REMAINDER_POPULATION): # realizamos varios torneos hasta tener la cantidad necesaria
+        contestants: list[Individual] = [] # contiene los participantes de un torneo
+        r = random.randint(1, REMAINDER_POPULATION) # número aleatorio de participantes en un torneo
+        maxFitness = 0
+        winner: Individual = Individual
+        
+        print("selecciono", r)
+
+        for _ in range(0, r): # se seleccionan los participantes aleatoriamente
+            i = random.randint(0, len(population)-1)
+            print("appendeo al numero", i)
+            contestants.append(population[i])
+
+        print("tengo estos contestants: ")
+        for contestant in contestants: print(contestant.chromosome)
+        print("hago torneo")
+
+        for contestant in contestants: # se realiza un torneo, el ganador es aquel con mayor fitness
+            if contestant.fitness > maxFitness:
+                maxFitness = contestant.fitness
+                winner = contestant
+        print("el ganador de este torneo tiene: ", winner.chromosome, winner.fitness)
+        selectedPossibleParents.append(winner) # el ganador del torneo puede reproducirse
+
+    print("terminamos con ")
+    for winner in selectedPossibleParents: print(winner.chromosome)
+
+    return selectedPossibleParents
